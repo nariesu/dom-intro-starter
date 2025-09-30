6c9ccffba2cb976688f027f70e1c776f8ad4135d
meredith <meredithsnavea1@gmail.com>
2025-09-30 14:42:19 -0700

    Updated Assignment

--- scripts.js
+++ scripts.js
@@ -21,7 +21,6 @@ for(let span of medBlueSpans){
     span.style.color = "#3C5E73";
 };
 
-
 // Let's select some SVG icons by a CSS selector and change the stroke color on these. For fun, let's create an array of the colors used in our document (find these in the root of the CSS document), and as we iterate through our icons, let's change each one to one of the colors in that array of colors from the document.
 let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
 
@@ -30,3 +29,6 @@ let svgs = document.querySelectorAll("#svgs svg");
 for(let i = 0; i < colors.length; i++) {
     svgs[i].style.stroke = colors[i];
 }
+
+// In this section, select the first of the spans with the class "bold" and change the text color to the light blue color from our CSS (#7C9EA6).
+document.querySelector(".bold").style.color = "#7C9EA6";
